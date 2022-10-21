eae3e22fed01fd058e8a07f93fab46ccde4a673d
saugatsigdel1 <sigdelsaugat2060@gmail.com>
2022-10-21 12:37:05 +0545

    js added

---
+++ firdt.js
@@ -0,0 +1,10 @@
+const text1 =document.getElementById('text1');
+
+text1.addEventListener('mouseover' ,function handleMouseOver() {
+text1.style.color='green';
+});
+
+
+text1.addEventListener('mouseout' ,function handleMouseOut() {
+text1.style.color='black';
+});
